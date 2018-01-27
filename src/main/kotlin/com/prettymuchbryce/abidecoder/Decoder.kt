bcf6bd56a48fb2f54b5e5330863d8ea292e342c8
Bryce Neal <brycedneal@gmail.com>
2018-01-26 17:18:16 -0800

    More work on method decoding

--- src/main/kotlin/com/prettymuchbryce/abidecoder/Decoder.kt
+++ src/main/kotlin/com/prettymuchbryce/abidecoder/Decoder.kt
@@ -1,133 +1,81 @@
 package com.prettymuchbryce.abidecoder 
 
-import org.web3j.protocol.Web3j
-import org.web3j.abi.Utils;
-import org.web3j.protocol.core.methods.response.AbiDefinition
-import org.web3j.protocol.http.HttpService
 import com.fasterxml.jackson.module.kotlin.*
+import org.spongycastle.util.encoders.Hex
+import org.ethereum.vm.trace.Serializers
+
+/*
+import org.web3j.abi.Utils;
 import org.web3j.abi.FunctionEncoder
 import org.web3j.abi.FunctionReturnDecoder
 import org.web3j.abi.TypeDecoder
 import org.web3j.abi.datatypes.Type
 import org.web3j.abi.datatypes.generated.AbiTypes
+*/
+
+import org.ethereum.solidity.Abi
+
 import java.util.stream.Collectors
-import org.web3j.utils.Numeric
-import org.web3j.crypto.Hash
 
 fun main(args: Array<String>) {
-	var web3: Web3j = Web3j.build(HttpService())
-	println("Hello, world!")
-	println(web3)
-
 	val json = """
 			[{"inputs": [{"type": "address", "name": ""}], "constant": true, "name": "isInstantiation", "payable": false, "outputs": [{"type": "bool", "name": ""}], "type": "function"}, {"inputs": [{"type": "address[]", "name": "_owners"}, {"type": "uint256", "name": "_required"}, {"type": "uint256", "name": "_dailyLimit"}], "constant": false, "name": "create", "payable": false, "outputs": [{"type": "address", "name": "wallet"}], "type": "function"}, {"inputs": [{"type": "address", "name": ""}, {"type": "uint256", "name": ""}], "constant": true, "name": "instantiations", "payable": false, "outputs": [{"type": "address", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "creator"}], "constant": true, "name": "getInstantiationCount", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"indexed": false, "type": "address", "name": "sender"}, {"indexed": false, "type": "address", "name": "instantiation"}], "type": "event", "name": "ContractInstantiation", "anonymous": false}]
 		"""
-
-	val mapper = jacksonObjectMapper()
-
-	var contracts: List<AbiDefinition> = mapper.readValue<List<AbiDefinition>>(json)
-	println(contracts[0].getName())
-}
-
-// fun decodeParams(types: List<String>, 
-
-/*
-SolidityCoder.prototype.decodeParams = function (types, bytes) {
-    var solidityTypes = this.getSolidityTypes(types);
-    var offsets = this.getOffsets(types, solidityTypes);
-
-    return solidityTypes.map(function (solidityType, index) {
-        return solidityType.decode(bytes, offsets[index],  types[index], index);
-    });
-};
-*/
-
-data class DecodedMethod(
-	val name: String,
-	val params: List<Param>
-);
-
-class Param constructor(namedType: AbiDefinition.NamedType, value: Type<*>) {
-	val _value = value
-	val _namedType = namedType
-
-	fun getValue(): Any {
-		return _value.getValue()
-	}
-
-	fun getName(): String {
-		return _namedType.getName()
-	}
-
-	fun getType(): String {
-		return _namedType.getType()
-	}
 }
 
 class Decoder {
-	val _savedAbis = mutableListOf<AbiDefinition>()
-	val _methodIDs: HashMap<String, AbiDefinition> = HashMap()
+	val _savedAbis = mutableListOf<Abi.Entry>()
+	val _methodIDs: HashMap<String, Abi.Entry> = HashMap()
 
-	private fun getTypesFromNamedTypes(input: String, namedTypes: List<AbiDefinition.NamedType>): List<Type<*>> {
-		val result = mutableListOf<Type<*>>()
-		for (namedType in namedTypes) {
-			val c = AbiTypes.getType(namedType.getType())
-			val type: Type<*> = c.javaClass()
-			result.append(type)
-		}
-		return result
-	}
-	
-	private fun buildMethodSignature(methodName: String, parameters: List<AbiDefinition.NamedType>): String {
-        val result = StringBuilder()
-        result.append(methodName);
-        result.append("(");
-        val params = parameters.stream()
-                .map{t -> t.getType()}
-                .collect(Collectors.joining(","));
-        result.append(params);
-        result.append(")");
-		val bytes = result.toString().toByteArray()
-		val hash = Hash.sha3(bytes)
-		return Numeric.toHexString(hash)
-	}
+	data class DecodedMethod (
+		val name: String,
+		val params: List<Param>
+	)
 
-    private fun buildMethodId(methodSignature: String): String {
-        val input = methodSignature.toByteArray();
-        val hash = Hash.sha3(input);
-        return Numeric.toHexString(hash).substring(0, 10);
-    }
+	data class Param (
+		val name: String,
+		val type: String,
+		val value: Any
+	)
 
-	fun addAbi (abis: List<AbiDefinition>) {
-		for (abi in abis) {
-			if (abi.getName() != null) {
-				val methodSignature = buildMethodSignature(abi.getName(), abi.getInputs())
-				if (abi.getType() == "event") {
-					_methodIDs[methodSignature.substring(0, 2)] = abi
-				} else {
-					_methodIDs[methodSignature.substring(2, 10)] = abi
-				}
+	fun addAbi (json: String) {
+		val abi: Abi = Abi.fromJson(json)
+		for (entry in abi) {
+			if (entry.name != null) {
+				val methodSignature = entry.encodeSignature()
+				_methodIDs[Hex.toHexString(methodSignature)] = entry
 			}
+			_savedAbis.add(entry)
 		}
-
-		_savedAbis.addAll(abis)
 	}
 
-	fun getAbis(): List<AbiDefinition> {
+	fun getAbis(): List<Abi.Entry> {
 		return _savedAbis
 	}
 
-	fun getMethodIDs(): Map<String, AbiDefinition> {
+	fun getMethodIDs(): Map<String, Abi.Entry> {
 		return _methodIDs	
 	}
 
-	fun decodeMethod(data: String): DecodedMethod {
-		val methodID = data.substring(2, 10)
-		val abiItem = _methodIDs[methodID]
-		if (abiItem != null) {
-			val params = abiItem.inputs
-			val decoded = FunctionReturnDecoder.decode(data, params)
+	fun decodeMethod(data: String): DecodedMethod? {
+		val noPrefix = data.removePrefix("0x")
+		val bytes = Hex.decode(noPrefix.toUpperCase())
+		val methodBytes: ByteArray = bytes.sliceArray(0..3)
+		val entry = _methodIDs[Hex.toHexString(methodBytes)]
+		if (entry is Abi.Function) {
+			val decoded = entry.decode(bytes)
+			val params = mutableListOf<Param>()
+			for (i in decoded.indices) {
+				val name = entry.inputs[i].name
+				val type = entry.inputs[i].type.toString()
+				// val value = Serializers.serializeFieldsOnly(decoded[i]!!, true)
+				val value = decoded[i]!!
+				val param = Param(name, type, value)
+				params.add(param)
+			}
+			return DecodedMethod(entry.name, params)
 		}
+		return null
 	}
 }
 
--- src/test/kotlin/com/prettymuchbryce/abidecoder/DecoderTests.kt
+++ src/test/kotlin/com/prettymuchbryce/abidecoder/DecoderTests.kt
@@ -1,16 +1,13 @@
 package com.prettymuchbryce.abidecoder
-
+import java.math.BigInteger;
 import org.junit.Assert.*
 import com.fasterxml.jackson.module.kotlin.*
-import org.web3j.protocol.core.methods.response.AbiDefinition
 import org.junit.Test
+import org.spongycastle.util.encoders.Hex;
 
 val testAbiJson= """
 			[{"inputs": [{"type": "address", "name": ""}], "constant": true, "name": "isInstantiation", "payable": false, "outputs": [{"type": "bool", "name": ""}], "type": "function"}, {"inputs": [{"type": "address[]", "name": "_owners"}, {"type": "uint256", "name": "_required"}, {"type": "uint256", "name": "_dailyLimit"}], "constant": false, "name": "create", "payable": false, "outputs": [{"type": "address", "name": "wallet"}], "type": "function"}, {"inputs": [{"type": "address", "name": ""}, {"type": "uint256", "name": ""}], "constant": true, "name": "instantiations", "payable": false, "outputs": [{"type": "address", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "creator"}], "constant": true, "name": "getInstantiationCount", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"indexed": false, "type": "address", "name": "sender"}, {"indexed": false, "type": "address", "name": "instantiation"}], "type": "event", "name": "ContractInstantiation", "anonymous": false}]
 		"""
-
-val mapper = jacksonObjectMapper()
-var contracts: List<AbiDefinition> = mapper.readValue<List<AbiDefinition>>(testAbiJson)
 
 class DecoderTests {
 	@Test
@@ -23,10 +20,55 @@ class DecoderTests {
 	@Test
 	fun addAbis() {
 		val decoder = Decoder()
-		decoder.addAbi(contracts)
+		decoder.addAbi(testAbiJson)
 		val abis = decoder.getAbis()
 		assertEquals(5, abis.size)
 		val methodIDs = decoder.getMethodIDs()
 		assertEquals(5, methodIDs.size)
 	}
+
+	@Test
+	fun decodeMethod() {
+    	val testData = "0x53d9d9100000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000a6d9c5f7d4de3cef51ad3b7235d79ccc95114de5000000000000000000000000a6d9c5f7d4de3cef51ad3b7235d79ccc95114daa";
+		val decoder = Decoder()
+		decoder.addAbi(testAbiJson)
+		val decodedMethod = decoder.decodeMethod(testData)!!
+		assertEquals(3, decodedMethod.params.size)
+		assertEquals("_owners", decodedMethod.params[0].name)
+		assertEquals("address[]", decodedMethod.params[0].type)
+		val mapper = jacksonObjectMapper()
+		var jsonStr = mapper.writeValueAsString(decodedMethod.params[0].value)
+		val array = decodedMethod.params[0].value
+		if (array is Array<*>) {
+			if (array[0] is ByteArray) {
+				assertEquals("0xa6d9c5f7d4de3cef51ad3b7235d79ccc95114de5", Hex.toHexString(array[0]))
+				print("here is it")
+				print(decodedMethod.params[0].value)
+			}
+		}
+		// print(jsonStr)
+		// print(JSON.stringify(decodedMethod.params[0].value))
+		print("WTF")
+		// assertEquals("0xa6d9c5f7d4de3cef51ad3b7235d79ccc95114de5", Hex.toHexString(decodedMethod.params[0].value)
+	}
 }
+/*
+  it('decode data', () => {
+    abiDecoder.addABI(testABI);
+    const testData = "0x53d9d9100000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000a6d9c5f7d4de3cef51ad3b7235d79ccc95114de5000000000000000000000000a6d9c5f7d4de3cef51ad3b7235d79ccc95114daa";
+    const decodedData = abiDecoder.decodeMethod(testData);
+    expect(decodedData).to.be.an('object');
+    expect(decodedData).to.have.all.keys('name', 'params');
+    expect(decodedData.name).to.be.a('string');
+    expect(decodedData.params).to.be.a('array');
+    expect(decodedData.params).to.have.length.of(3);
+    expect(decodedData.params[0].value).to.deep.equal(['0xa6d9c5f7d4de3cef51ad3b7235d79ccc95114de5', '0xa6d9c5f7d4de3cef51ad3b7235d79ccc95114daa']);
+    expect(decodedData.params[0].name).to.equal('_owners');
+    expect(decodedData.params[0].type).to.equal('address[]');
+    expect(decodedData.params[1].value).to.equal('1');
+    expect(decodedData.params[1].name).to.equal('_required');
+    expect(decodedData.params[1].type).to.equal('uint256');
+    expect(decodedData.params[2].value).to.equal('0');
+    expect(decodedData.params[2].name).to.equal('_dailyLimit');
+    expect(decodedData.params[2].type).to.equal('uint256');
+  });*/
