3ae24ecb55dec214c19e539a1439bf75663ce1e0
Bryce Neal <brycedneal@gmail.com>
2018-01-31 11:24:05 -0800

    Update DecoderTests.kt

--- src/test/kotlin/com/prettymuchbryce/abidecoder/DecoderTests.kt
+++ src/test/kotlin/com/prettymuchbryce/abidecoder/DecoderTests.kt
@@ -1,9 +1,10 @@
 package com.prettymuchbryce.abidecoder
-import java.math.BigInteger;
+
+import java.math.BigInteger
 import org.junit.Assert.*
 import com.fasterxml.jackson.module.kotlin.*
 import org.junit.Test
-import org.spongycastle.util.encoders.Hex;
+import org.spongycastle.util.encoders.Hex
 
 val testAbiJson= """
 			[{"inputs": [{"type": "address", "name": ""}], "constant": true, "name": "isInstantiation", "payable": false, "outputs": [{"type": "bool", "name": ""}], "type": "function"}, {"inputs": [{"type": "address[]", "name": "_owners"}, {"type": "uint256", "name": "_required"}, {"type": "uint256", "name": "_dailyLimit"}], "constant": false, "name": "create", "payable": false, "outputs": [{"type": "address", "name": "wallet"}], "type": "function"}, {"inputs": [{"type": "address", "name": ""}, {"type": "uint256", "name": ""}], "constant": true, "name": "instantiations", "payable": false, "outputs": [{"type": "address", "name": ""}], "type": "function"}, {"inputs": [{"type": "address", "name": "creator"}], "constant": true, "name": "getInstantiationCount", "payable": false, "outputs": [{"type": "uint256", "name": ""}], "type": "function"}, {"inputs": [{"indexed": false, "type": "address", "name": "sender"}, {"indexed": false, "type": "address", "name": "instantiation"}], "type": "event", "name": "ContractInstantiation", "anonymous": false}]
